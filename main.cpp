a070f773a8f2e16e04ad01d9c9af76d0004d16a6
Yang Liu <summerliu2333@gmail.com>
2026-04-24 15:49:14 -0700

    Milestone 3

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 const int NUM_LANES = 4;
-const int INITIAL_CARS = 2;
+const int INIT_PER_LANE = 2;
 
 const double LEAVE_PROB = 0.46;
 const double JOIN_PROB = 0.39;
@@ -19,9 +19,22 @@ int main() {
 
     vector<deque<Car>> lanes(NUM_LANES);
 
-    lanes[0].push_back(Car());
+    for (int i = 0; i < NUM_LANES; i++) {
+        for (int j = 0; j < INIT_PER_LANE; j++) {
+            lanes[i].push_back(Car());
+        }
+    }
 
-    cout << "M2: 4 lanes created successfully\n";
+    cout << "Initial queue:" << endl;
+    for (int i = 0; i < NUM_LANES; i++) {
+        cout << "Lane " << i+1 << ":" << endl;
+        for (auto& car : lanes[i]) {
+            cout << "    ";
+            car.print();
+            cout << endl;
+        }
+    }
+    cout << endl;
 
     return 0;
 }
