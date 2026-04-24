fc3cdba0dafe59a01394f2dd6faa414ae2c3014a
Yang Liu <summerliu2333@gmail.com>
2026-04-24 16:00:48 -0700

    M4(main loop & 50/50 empty lane logic)

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 const int NUM_LANES = 4;
 const int INIT_PER_LANE = 2;
+const int SIM_TIME = 20;
 
 const double LEAVE_PROB = 0.46;
 const double JOIN_PROB = 0.39;
@@ -35,6 +36,37 @@ int main() {
         }
     }
     cout << endl;
+
+    for (int time = 1; time <= SIM_TIME; time++) {
+        cout << "Time: " << time << endl;
+
+        for (int i = 0; i < NUM_LANES; i++) {
+            auto& lane = lanes[i];
+
+            if (lane.empty()) {
+                if (rand() % 2 == 0) {
+                    lane.push_back(Car());
+                    cout << "Lane " << i+1 << " Joined (empty lane)\n";
+                }
+            } else {
+                double r = (double)rand() / RAND_MAX;
+                if (r < 0.5) {
+                    lane.pop_front();
+                    cout << "Lane " << i+1 << " Paid\n";
+                } else {
+                    lane.push_back(Car());
+                    cout << "Lane " << i+1 << " Joined\n";
+                }
+            }
+        }
+
+        for (int i = 0; i < NUM_LANES; i++) {
+            cout << "Lane " << i+1 << " Queue:\n";
+            if (lanes[i].empty()) cout << "    Empty\n";
+            else for (auto& c : lanes[i]) { cout << "    "; c.print(); cout << endl; }
+        }
+        cout << endl;
+    }
 
     return 0;
 }
