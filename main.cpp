882f7aaa3740017fe71367d974a7c6d08ad32f4f
Yang Liu <summerliu2333@gmail.com>
2026-04-24 13:58:36 -0700

    Milestone 1

--- main.cpp
+++ main.cpp
@@ -10,5 +10,20 @@ const int INITIAL_CARS = 2;
 const double LEAVE_PROB = 0.55;
 
 int main() {
+    srand(time(0));
+
+    deque<Car> tollLane;
+
+    for (int i = 0; i < INITIAL_CARS; i++) {
+        tollLane.push_back(Car());
+    }
+
+    cout << "Initial queue:" << endl;
+    for (auto& car : tollLane) {
+        car.print();
+        cout << endl;
+    }
+    cout << endl;
+
     return 0;
 }
